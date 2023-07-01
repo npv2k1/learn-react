3329a7064588be202f15181ff36fafdc4eb0dfcb
Phạm Văn Nguyên <npv2k1@gmail.com>
2023-07-02 00:13:23 +0700

    Add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
 
 function App() {
   const [count, setCount] = useState(0);
-  console.log("🚀 ~ file: App.tsx:7 ~ App ~ count:", count);
+  console.log('🚀 ~ file: App.tsx:7 ~ App ~ count:', count);
 
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
+    <div className="flex h-screen w-screen items-center justify-center">
       <h1>Hello, world!</h1>
     </div>
   );
