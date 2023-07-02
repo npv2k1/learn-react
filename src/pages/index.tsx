54ed8c8afa18745207eca89c696bb67d53e6f043
Phạm Văn Nguyên <npv2k1@gmail.com>
2023-07-02 11:41:48 +0700

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,8 +13,8 @@ const HomePage = () => {
       .then((text) => setDocs(text));
   }, []);
   return (
-    <div className="flex w-screen items-center justify-center">
-      <article className="prose overflow-y-auto p-5 lg:prose-xl">
+    <div className="flex items-center justify-center overflow-auto">
+      <article className="prose h-full w-full overflow-y-auto p-5 lg:prose-xl">
         <ReactMarkdown children={docs} remarkPlugins={[remarkGfm]} />
       </article>
     </div>
